e1f70ac80b2acb54c87846cb2110770f80d8bcd9
KSB <green@ksb-sendai.co.jp>
2019-09-17 21:10:17 +0900

    [add] rest API methods

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -11,8 +11,12 @@
         span.under お知らせ
     .space-small
     .newsArea
+        //- TODO: function.phpを書き換えて最新の記事4件を取得出来るようにする。カスタムwp-jsonの取得また、カテゴリーを取れるようにする。
+        .newsList(v-for='post of ksbNews')
+            a(:href='post.link') {{ post.title.rendered }}
+            p {{ post.date }}
 
-    .space-wide
+    .space-widh
 
 
 
@@ -21,11 +25,35 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import Quasar from 'quasar';
+import axios from 'axios';
 
 @Component
 
 export default class Homepage extends Vue {
     private slide = 1;
+    // axiosで取得したデータを入れる配列
+    private ksbNews = [];
+    // 写真等のメディアを入れる配列
+    private media = [];
+    // エラーを入れる配列
+    private error = [];
+    // wp-jsonのURL
+    private postURL: string = 'http://ksbnews.local/wp-json/wp/v2/posts/';
+
+    private async getPosts() {
+        try {
+            await axios
+                .get(this.postURL)
+                .then((Response) => { this.ksbNews = Response.data; });
+        } catch (error) {
+            console.log('データ取得できませんでした。', error);
+        }
+    }
+
+    private created() {
+        this.getPosts();
+    }
+
 }
 </script>
 
