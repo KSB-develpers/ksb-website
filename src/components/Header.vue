19aee3737db18b61977c41208d20f55ee2cd2700
KSB <green@ksb-sendai.co.jp>
2019-09-20 11:22:13 +0900

    [add] mobile-menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,32 @@
 <template lang="pug">
 #header
+    .mobile-menu(v-if='menuStatus')
+        q-toolbar
+            q-space/
+            q-btn(dense flat round icon='close' @click='menuActive')
+        p.floating-menu-title - Menu -
+        .floating-menu
+            router-link(to='/')
+                .menu-panel
+                    p ホーム
+            router-link(to='/about')
+                .menu-panel
+                    p 会社概要
+            .menu-panel
+                p 事業内容
+            .menu-panel
+                p 採用情報
+            .menu-panel
+                p お問い合わせ
+        .floating-logo
+            img(:src="headerLogo" height=90 width=150)
     //- ヘッダーの実装部分
     .desktop-header
         q-header(reveal :reveal-offset="100")
             .navbar-parent
                 .navbar
                     router-link(to='/' tag='a')
-                        img(:src="headerLogo")
+                        img(:src="headerLogo" height=60 width=100)
                     .nav-item
                         router-link(to='/')
                             q-btn(flat size='1.2rem').item-style ホーム
@@ -24,9 +44,7 @@
                     router-link(to='/')
                         img.mobile-logo(:src="headerLogo")
                     q-space/
-                    q-btn.mobile-btn(dense flat round icon='menu')
-
-
+                    q-btn.mobile-btn(dense flat round icon='menu' @click='menuActive')
 
 </template>
 
@@ -35,7 +53,11 @@ import { Vue, Component } from 'vue-property-decorator';
 @Component
 
 export default class Header extends Vue {
-    private headerLogo = require('@/assets/navbar-logo.png');
+    private headerLogo = require('@/assets/ksb-logo.svg');
+    private menuStatus = false;
+    private menuActive() {
+        this.menuStatus = !this.menuStatus;
+    }
 }
 </script>
 
@@ -61,7 +83,8 @@ a
     font-weight bold
 .mobile-header
     display none
-
+.mobile-menu
+    display none
 @media screen and (max-width: 780px)
     .desktop-header
         display none
@@ -75,4 +98,34 @@ a
         max-width 100%
     .mobile-logo
         height 50px
+    .mobile-menu
+        display block
+        position fixed
+        height 100%
+        width 100%
+        z-index 9999
+        background-color #FFFFFF
+    .floating-menu-title
+        margin 50px 0 50px 0
+        font-size 3em
+        text-align center
+    .floating-menu
+        height auto
+        width 90%
+        margin 0 auto
+        margin-bottom 50px
+    .menu-panel
+        height 70px
+        display flex
+        justify-content center
+        align-items center
+        font-size 20px
+        border-top 1px dotted #E0E0E0
+        border-bottom 1px dotted #E0E0E0
+        p
+            margin 0
+            color black
+    .floating-logo
+        text-align center
+
 </style>
